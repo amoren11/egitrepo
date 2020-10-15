d9170fb240db8788fbd052984597eec2a04a0ad8
Aaron <Aaron@DESKTOP-KQ72MI4>
2020-10-15 14:41:22 -0600

    'new feature added'

--- EgitTesting/src/HelloWorld.java
+++ EgitTesting/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("New line has been added");
+		System.out.println("New feature addition");
 		
 	}
 
